9f7e30c92487804f7b79c206223f9a04bcde0109
EBregains <ebregains@gmail.com>
2024-02-20 17:26:47 -0300

    Middleware Info Logger addition

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::{configuration::get_config, startup::run};
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+  // Setup Logger to imrpove Observability
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
   // Panic if we can't read configuration
   let configuration = get_config().expect("Failed to read configuration.");
   // Get port from config
