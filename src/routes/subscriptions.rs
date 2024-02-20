cc4fd2818157954ae73413e552e9ec20543f4950
EBregains <ebregains@gmail.com>
2024-02-20 19:22:04 -0300

    Switched from Log to Tracing dependency

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,18 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+  let request_id = Uuid::new_v4();
+  tracing::info!("
+    [RequestID {}] Adding '{}' <{}> as a new subscriber.",
+    request_id,
+    form.name,
+    form.email
+  );
+  tracing::info!("
+    [RequestID {}] Saving new subscriber details in the databse.",
+    request_id,
+  );
+  
   match sqlx::query!(
     r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,9 +37,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
   .execute(pool.get_ref())
   .await
   {
-    Ok(_) => HttpResponse::Ok().finish(),
+    Ok(_) =>  {
+      tracing::info!("[RequestID {}] New subscriber details have been saved", request_id);
+      HttpResponse::Ok().finish()
+    },
     Err(e) => {
-      println!("Failed to execute query: {}", e);
+      tracing::error!("[RequestID {}]Failed to execute query: {:?}", request_id, e);
       HttpResponse::InternalServerError().finish()
     }
   }
