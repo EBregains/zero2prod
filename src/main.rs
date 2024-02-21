6a411e49360b3332ad2588b19795206d31848de6
EBregains <ebregains@gmail.com>
2024-02-21 20:24:41 -0300

    New file for telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::{configuration::get_config, startup::run};
-use env_logger::Env;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
   // Setup Logger to imrpove Observability
-  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+  let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+  init_subscriber(subscriber);
   // Panic if we can't read configuration
   let configuration = get_config().expect("Failed to read configuration.");
   // Get port from config
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,31 +7,45 @@
 use std::net::TcpListener;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
-use zero2prod::configuration::{get_config, DatabaseSettings};
+use zero2prod::{configuration::{get_config, DatabaseSettings}, telemetry::{get_subscriber, init_subscriber}};
+use once_cell::sync::Lazy;
 
+// Ensure that the `tracing` stack is only initialised once using `once_cell
+static TRACING: Lazy<()> = Lazy::new(|| {
+  let default_filter_level = "info".to_string();
+  let subscriber_name = "test".to_string();
+  if std::env::var("TEST_LOG").is_ok() {
+    let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+    init_subscriber(subscriber);
+  } else {
+    let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+    init_subscriber(subscriber);
+  }
+});
 pub struct TestApp {
   pub address: String,
   pub db_pool: PgPool
 }
 // Launch the application in the background
 async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let address =     format!("http://127.0.0.1:{}", port);
+  Lazy::force(&TRACING);
+  let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+  let port = listener.local_addr().unwrap().port();
+  let address =     format!("http://127.0.0.1:{}", port);
 
-    let mut configuration = get_config().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database)
-      .await;
-    let server = zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
-    TestApp {
-      address,
-      db_pool: connection_pool,
-    }
+  let mut configuration = get_config().expect("Failed to read configuration.");
+  configuration.database.database_name = Uuid::new_v4().to_string();
+  let connection_pool = configure_database(&configuration.database)
+    .await;
+  let server = zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
+  // Launch the server as a background task
+  // tokio::spawn returns a handle to the spawned future,
+  // but we have no use for it here, hence the non-binding let
+  let _ = tokio::spawn(server);
+  TestApp {
+    address,
+    db_pool: connection_pool,
+  }
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
@@ -72,7 +86,6 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
